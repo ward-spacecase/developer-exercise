22e9f64ea6a250f9037903f25c97b99f4b704eaf
Andrew <ward.spacecase@gmail.com>
2024-11-23 15:48:30 -0700

    marklar passing

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,39 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+
+    words = str.split
+    new_sentence = ""
+
+    for word in words
+      punctuation = ""
+
+      # handle puctuation
+      if word[-1].match?(/[[:punct:]]/)
+        punctuation = word[-1]
+        word.chop!
+      end
+
+      if word.length > 4
+        # handle capital
+        if word =~ /[A-Z]/
+          new_sentence << "Marklar"
+        else
+          new_sentence << "marklar"
+        end
+
+      else
+        new_sentence << word
+      end
+
+      if punctuation.length >= 1
+        new_sentence << punctuation
+      end
+
+      new_sentence << " "
+    end
+
+    return new_sentence.chop
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
